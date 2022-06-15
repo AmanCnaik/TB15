8a954670d915a87a93bed5d338fd5efa2a3bd009
mukes <mukes@DESKTOP-GMLVQEM>
2022-06-15 14:56:13 +0530

    By ABC

--- gittb15/src/gittb15/Demo.java
+++ gittb15/src/gittb15/Demo.java
@@ -2,7 +2,15 @@ package gittb15;
 
 public class Demo 
 {
-
+  public void m1()
+  
+  {
+	  
+	  System.out.println("Hello This is M1");
+  }
+	
+	
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello");
